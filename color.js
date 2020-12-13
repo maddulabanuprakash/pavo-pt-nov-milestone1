314b8cd3709adc8fe841f7c6e35352d30df0531b
banuprakash <maddulabanuprakash@gmail.com>
2020-12-13 20:11:00 +0530

    Add color.js

---
+++ color.js
@@ -0,0 +1,8 @@
+self.color = function () {
+    return fake('{{commerce.color}}');
+  };
+
+  self.color.schema = {
+    "description": "Generates a color",
+    "sampleResults": ["red", "white", "black"]
+  };
